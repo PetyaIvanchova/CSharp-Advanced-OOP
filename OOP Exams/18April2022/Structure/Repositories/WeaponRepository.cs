5edbca46f527f3954a5ea3d349fe564273e2bdfa
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2022-08-20 16:26:12 +0300

    Add files via upload

---
+++ OOP Exams/18April2022/Structure/Repositories/WeaponRepository.cs
@@ -0,0 +1,29 @@
+﻿using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes.Repositories
+{
+    class WeaponRepository : IRepository<IWeapon>
+    {
+        private List<IWeapon> weapons;
+
+        public WeaponRepository()
+        {
+            weapons = new List<IWeapon>();
+        }
+
+        public IReadOnlyCollection<IWeapon> Models { get => weapons.AsReadOnly(); }
+
+        public void Add ( IWeapon model ) => weapons.Add (model);
+        public IWeapon FindByName ( string name ) => weapons.FirstOrDefault (x => x.Name==name);
+        public bool Remove ( IWeapon model )
+        {
+            return   weapons.Remove( model);           
+        }
+    }
+}
+
