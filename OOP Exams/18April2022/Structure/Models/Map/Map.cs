e076b8ff00f9ed7cce66b1fb1d468a1b3eae2a31
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2022-08-21 11:46:48 +0300

    Add files via upload

---
+++ OOP Exams/18April2022/Structure/Models/Map/Map.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Map
+{
+    using Models.Contracts;
+    using Models.Heroes;
+    public class Map : IMap
+    {
+        //public string Fight(ICollection<IHero> players)
+        //{
+        //    List<Knight> kn = new List<Knight>();
+        //    List<Barbarian> ba = new List<Barbarian>();
+        //    int countK = 0;
+        //    int countB = 0;
+        //    foreach (var item in players)
+        //    {
+        //        if (item is Knight)
+        //        {
+        //            kn.Add((Knight)item);
+        //            if (((Knight)item).IsAlive)
+        //            {
+        //                countK++;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            ba.Add((Barbarian)item);
+        //            if (((Barbarian)item).IsAlive)
+        //            {
+        //                countB++;
+        //            }
+        //        }
+        //    }
+
+        //    while (countB > 0 && countK > 0)
+        //    {
+        //        foreach (var item in kn)
+        //        {
+        //            if (item.IsAlive)
+        //            {
+        //                foreach (var item1 in ba)
+        //                {
+        //                    int weapondamage = item.Weapon.DoDamage();
+        //                    item1.TakeDamage(weapondamage);
+        //                    if (!item1.IsAlive)
+        //                    {
+        //                        countB--;
+        //                        if (countB==0)break;
+        //                                       }
+        //                }
+        //            }
+        //            if (countB == 0)
+        //            {
+        //                break;
+        //            }
+        //        }
+        //        foreach (var item in ba)
+        //        {
+        //            if (item.IsAlive)
+        //            {
+        //                foreach (var item1 in kn)
+        //                {
+        //                    int weapondamage = item.Weapon.DoDamage();
+        //                    item1.TakeDamage(weapondamage);                            
+        //                    if (!item1.IsAlive )
+        //                    {
+        //                        countK--;
+        //                        if (countK == 0)break;                                
+        //                    }
+        //                }
+        //            }
+        //            if (countK == 0)
+        //            {
+        //                break;
+        //            }
+        //        }
+
+        //    }
+
+        //  	if (countK>0)
+        //    {
+        //        return $"The knights took { kn.Count - countK } casualties but won the battle.";
+        //    }
+        //    else
+        //    {
+        //       return $"The barbarians took {ba.Count-countB} casualties but won the battle.";
+        //    }
+
+
+
+        //}
+        public string Fight ( ICollection<IHero> players )
+        {
+            List<Knight> kn = new List<Knight> ();
+            List<Barbarian> ba = new List<Barbarian> ();
+            int countK = 0;
+            int countB = 0;
+            foreach ( var item in players )
+            {
+                if ( item is Knight )
+                {
+                    kn.Add ((Knight) item);
+                    if ( ( (Knight) item ).IsAlive )
+                    {
+                        countK++;
+                    }
+                }
+                else
+                {
+                    ba.Add ((Barbarian) item);
+                    if ( ( (Barbarian) item ).IsAlive )
+                    {
+                        countB++;
+                    }
+                }
+            }
+
+            while ( countB > 0 && countK > 0 )
+            {
+                foreach ( var item in kn )
+                {
+                    if ( item.IsAlive )
+                    {
+
+                        foreach ( var item1 in ba )
+                        {
+                            if ( item1.IsAlive )
+                            {
+                                int weapondamage = item.Weapon.DoDamage ();
+                                item1.TakeDamage (weapondamage);
+                                if ( !item1.IsAlive )
+                                {
+                                    countB--;
+
+                                    if ( countB == 0 ) return $"The knights took {kn.Count - countK} casualties but won the battle.";
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+                foreach ( var item in ba )
+                {
+                    if ( item.IsAlive )
+                    {
+
+                        foreach ( var item1 in kn )
+                        {
+                            if ( item1.IsAlive )
+                            {
+                                int weapondamage = item.Weapon.DoDamage ();
+                                item1.TakeDamage (weapondamage);
+                                if ( !item1.IsAlive )
+                                {
+                                    countK--;
+                                    if ( countK == 0 ) return $"The barbarians took {ba.Count - countB} casualties but won the battle.";
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+
+            }
+            return $"The barbarians took {ba.Count - countB} casualties but won the battle.";
+
+        }
+    }
+}
