12a8b3be99b49918bca9112c232db826a31bd8d7
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2022-08-21 11:50:04 +0300

    Add files via upload

---
+++ OOP Exams/18April2022/Structure/Repositories/HeroRepository.cs
@@ -0,0 +1,38 @@
+﻿using Heroes.Models.Contracts;
+using Heroes.Models.Heroes;
+using Heroes.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes.Repositories
+{
+
+
+
+    public class HeroRepository : IRepository<IHero>
+    {
+        private List<IHero> models;
+
+        public HeroRepository ()
+        {
+            models = new List<IHero> ();
+        }
+
+        public IReadOnlyCollection<IHero> Models { get => models; }
+
+        public void Add ( IHero model ) => models.Add (model);
+        public IHero FindByName ( string name ) => models.FirstOrDefault (x => x.Name == name);
+        public bool Remove ( IHero model )
+        {
+            if ( models.Contains (model) )
+            {
+                models.Remove (model);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ OOP Exams/18April2022/Structure/Repositories/WeaponRepository.cs
@@ -0,0 +1,29 @@
+﻿using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes.Repositories
+{
+    class WeaponRepository : IRepository<IWeapon>
+    {
+        private List<IWeapon> models;
+        public IReadOnlyCollection<IWeapon> Models { get => models; }
+
+        public void Add ( IWeapon model ) => models.Add (model);
+        public IWeapon FindByName ( string name ) => models.FirstOrDefault (x => x.Name == name);
+        public bool Remove ( IWeapon model )
+        {
+            var p = models.FirstOrDefault (x => x.Name == model.Name && x.Durability == model.Durability);
+            if ( p != null )
+            {
+                models.Remove (p);
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
