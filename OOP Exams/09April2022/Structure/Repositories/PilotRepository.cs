63c104cfae2502d3aecb3d434f6e7c850c3cf988
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2022-08-21 12:18:40 +0300

    Add files via upload

---
+++ OOP Exams/09April2022/Structure/Repositories/PilotRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Repositories
+{
+    using Repositories.Contracts;
+    using Models.Contracts;
+    using System.Linq;
+
+    public class PilotRepository : IRepository<IPilot>
+    {
+        private List<IPilot> models;
+        public IReadOnlyCollection<IPilot> Models => models.AsReadOnly();
+
+        public void Add(IPilot model)
+        {
+            models.Add(model);
+        }
+
+        public IPilot FindByName(string name)
+        {
+            var sth = models.FirstOrDefault(x => x.FullName == name);
+            if (sth != null)
+            {
+                return sth;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool Remove(IPilot model)
+        {
+            if (models.Remove(model))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ OOP Exams/09April2022/Structure/Repositories/RaceRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Repositories
+{
+    using Models.Contracts;
+    using Repositories.Contracts;
+    using System.Linq;
+
+    public class RaceRepository : IRepository<IRace>
+    {
+        private List<IRace> models;
+        public IReadOnlyCollection<IRace> Models =>models.AsReadOnly();
+
+        public RaceRepository()
+        {
+            models = new List<IRace>();
+        }
+        public void Add(IRace model)
+        {
+            models.Add(model);
+        }
+
+        public IRace FindByName(string name)
+        {
+            var sth = models.FirstOrDefault(x => x.RaceName == name);
+            if (sth != null)
+            {
+                return sth;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool Remove(IRace model)
+        {
+            if (models.Remove(model))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
