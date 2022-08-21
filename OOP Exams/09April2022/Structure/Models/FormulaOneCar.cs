d088f9461a87ea875a168ff58ab4f4972ece428b
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2022-08-21 12:16:17 +0300

    Add files via upload

---
+++ OOP Exams/09April2022/Structure/Models/FormulaOneCar.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    using Models.Contracts;
+    public abstract class FormulaOneCar : IFormulaOneCar
+    {
+        private string model;
+        public string Model
+        {
+            get { return this.model; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length<3)
+                {
+                    throw new ArgumentException($"Invalid car model: { value }.");
+                }
+                this.model = value;
+
+            }
+        }
+        private int horsepower;
+        public int Horsepower
+        {
+            get { return this.horsepower; }
+           private set
+            {
+                if (value<900 || value>1050)
+                {
+                    throw new ArgumentException($"Invalid car horsepower: { value }.");
+                }
+                this.horsepower = value;
+            }
+        }
+        private double enginedisplacement;
+        public double EngineDisplacement
+        {
+            get { return this.enginedisplacement; }
+           private set
+            {
+                if (value<1.6 || value>2.00)
+                {
+                    throw new ArgumentException($"Invalid car engine displacement: { value }.");
+                }
+                this.enginedisplacement = value;
+            }
+        }
+        public FormulaOneCar(string model, int horsepower, double engineDisplacement)
+        {
+            Model = model;
+            Horsepower = horsepower;
+            EngineDisplacement = engineDisplacement;
+        }
+
+        public double RaceScoreCalculator(int laps)
+        {
+            return EngineDisplacement / Horsepower * laps;
+        }
+
+    }
+}
---
+++ OOP Exams/09April2022/Structure/Models/Pilot.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    using Models.Contracts;
+    public class Pilot : IPilot
+    {
+        private string fullname;
+        public string FullName
+        {
+            get { return this.fullname; }
+           private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length<5)
+                {
+                    throw new ArgumentException($"Invalid pilot name: { value }.");
+                }
+                this.fullname = value;
+            }
+        }
+
+        public IFormulaOneCar Car
+        {
+            get { return Car; }
+            private set
+            {
+                if (value==null)
+                {
+                    throw new NullReferenceException("Pilot car can not be null.");
+                }
+                this.Car = value;
+            }
+        }
+
+        public int NumberOfWins { get; private set; }
+
+        private bool canrace = false;
+        public bool CanRace
+        {
+            get { return this.canrace; }
+            private set { canrace = value; }
+        }
+       
+        public void AddCar(IFormulaOneCar car)
+        {
+            Car = car;
+            CanRace = true;
+        }
+        public Pilot(string fullname)
+        {
+            FullName = fullname;
+        }
+        public void WinRace()
+        {
+            NumberOfWins++;
+         
+        }
+        public override string ToString()
+        {
+            return $"Pilot { FullName } has { NumberOfWins } wins.";
+        }
+    }
+}
