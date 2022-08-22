3d8e53876b3767b796ee1f49d7a94a6ee0a5473c
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2022-08-22 20:26:14 +0300

    Add files via upload

---
+++ OOP Exams/22August2021/Business Logic/Models/Planets/Planet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Planets
+{
+    using Models.Planets.Contracts;
+    public class Planet : IPlanet
+    {
+        private List<string> items;
+        public ICollection<string> Items => items;
+
+        public Planet(string name)
+        {
+            Name = name;
+            items = new List<string>();
+        }
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Invalid name!");
+                }
+                this.name = value;
+            }
+        }
+    }
+}
