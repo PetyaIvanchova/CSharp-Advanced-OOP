75320d1efe300a3274ef1b809439ddd967930da8
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2022-08-22 20:23:06 +0300

    Add files via upload

---
+++ OOP Exams/22August2021/Business Logic/Models/Mission/Mission.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Mission
+{
+    using Models.Mission.Contracts;
+    using SpaceStation.Models.Astronauts.Contracts;
+    using SpaceStation.Models.Planets.Contracts;
+    using System.Linq;
+    using Models.Bags;
+
+    public class Mission : IMission
+    {
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        {
+            foreach (var ast in astronauts)
+            {
+                if (ast.Oxygen > 0)
+                {
+                    List<string> plItems = new List<string>();
+                    foreach (var item in planet.Items)
+                    {
+                        ast.Breath();
+                        ast.Bag.Items.Add(item);
+                        plItems.Add(item);
+                        if (ast.Oxygen == 0)
+                        {
+                            break;
+                        }
+                    }
+                    foreach (var item in plItems)
+                    {
+                        planet.Items.Remove(item);
+                    }
+
+                }
+            }
+        }
+    }
+}
