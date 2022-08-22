7847d831e32f8d4ee53580a4d0eec272f4554610
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2022-08-22 19:40:07 +0300

    Add files via upload

---
+++ OOP Exams/22August2021/Structure/Models/Mission/Mission.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Mission
+{
+    using Models.Mission.Contracts;
+    using SpaceStation.Models.Astronauts.Contracts;
+    using SpaceStation.Models.Planets.Contracts;
+    using System.Linq;
+
+    public class Mission : IMission
+    {
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        {
+            foreach (var ast in astronauts)
+            {
+                if (ast.Oxygen>0)
+                {
+
+                    var itemFound = planet.Items.FirstOrDefault();
+                    if (itemFound != null)
+                    {
+                        ast.Bag.Items.Add(itemFound);
+                        ast.Breath();
+                        planet.Items.Remove(itemFound);
+                    }
+                    if (planet.Items.Count == 0) break;
+
+                }
+            }
+        }
+    }
+}
