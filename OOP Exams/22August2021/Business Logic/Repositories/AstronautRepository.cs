9e59ee34587279c9f3a990cebb769d7caf7fa860
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2022-08-22 20:29:22 +0300

    Add files via upload

---
+++ OOP Exams/22August2021/Business Logic/Repositories/AstronautRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Repositories
+{
+    using Repositories.Contracts;
+    using Models.Astronauts.Contracts;
+    using System.Linq;
+
+    public class AstronautRepository : IRepository<IAstronaut>
+    {
+        public IReadOnlyCollection<IAstronaut> Models => models.AsReadOnly();
+        private List<IAstronaut> models;
+        public AstronautRepository()
+        {
+            models = new List<IAstronaut>();
+        }
+        public void Add(IAstronaut model)
+        {
+            models.Add(model);
+        }
+
+        public IAstronaut FindByName(string name)
+        {
+          return models.FirstOrDefault(x => x.Name == name);
+           
+        }
+
+        public bool Remove(IAstronaut model)
+        {
+            if (models.Remove(model))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OOP Exams/22August2021/Business Logic/Repositories/PlanetRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Repositories
+{
+    using Repositories.Contracts;
+    using Models.Planets.Contracts;
+    using System.Linq;
+
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        private List<IPlanet> models;
+        public IReadOnlyCollection<IPlanet> Models => models;
+
+        public PlanetRepository()
+        {
+            models = new List<IPlanet>();
+        }
+        public void Add(IPlanet model)
+        {
+            models.Add(model);
+        }
+
+        public IPlanet FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+
+        public bool Remove(IPlanet model)
+        {
+            if (models.Remove(model))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
