ef27b669476ea2955259234f9a2fc4e4b12aa03e
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2022-08-22 19:36:34 +0300

    Add files via upload

---
+++ OOP Exams/22August2021/Structure/Models/Bags/Backpack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Bags
+{
+    using Models.Bags.Contracts;
+    public class Backpack : IBag
+    {
+        private List<string> items;
+        public ICollection<string> Items => items.AsReadOnly();
+        public Backpack()
+        {
+            items = new List<string>();
+        }
+    }
+}
